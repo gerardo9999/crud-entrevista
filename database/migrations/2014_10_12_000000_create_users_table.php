f71b10cc0ad1c1d2fe9f506a50c96fed4530a40d
gerardodev2021 <gerardodev2021@gmail.com>
2021-10-01 14:22:25 -0400

    primer commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('nombre');
+            $table->string('apellidos')->nullable();
+            $table->date('fechaValid')->nullable()->default(date('Y-m-d'));
+            $table->string('telefono')->nullable();
+            $table->boolean('estado')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
